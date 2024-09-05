c1602880285f5dea7e7248d3947bf0e4bc607656
ALUNO <renanmcardozo@yahoo.com.br>
2024-09-04 21:14:04 -0300

    Primeiros passos

---
+++ AgendaTelefonica/Models/Agenda.cs
@@ -0,0 +1,14 @@
+namespace AgendaTelefonica.Models;
+
+public class Agenda
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Email { get; set; }
+    public string Telefone { get; set; }
+
+    public Agenda()
+    {
+        
+    }
+}
